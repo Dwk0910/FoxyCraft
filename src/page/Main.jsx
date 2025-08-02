a1f6bb269772f7c305b99b05bb2d44be08d588d0
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-08-03 00:14:01 +0900

    등록 서버 없을떄의 Main page 완료
    - 로고 변경 (non-license)
    - Context 추가하여 다른 메서드에서도 changeMenu 접근 가능하도록 만듦

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,10 +1,26 @@
 
+import { useContext } from "react";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { FiPlus } from "react-icons/fi";
+import { MenuContext } from "../App";
+
+import AddServer from "./AddServer";
+
 export default function Main() {
+    const { changeMenu } = useContext(MenuContext);
     return (
         <div className={"flex min-h-screen flex-col p-5"}>
-            <span className={"rounded-xl border-gray-600 border-2 h-auto p-3"}>
-                <span className={"h-5 text-2xl bold font-suite"}>안녕하세요!</span>
-            </span>
+            <div className={"flex flex-col justify-center items-center flex-grow"}>
+                <img src={"../../assets/images/icon.png"} alt={"bigicon"} className={"h-50"}/>
+                <span className={"h-5 text-2xl font-bold font-SeoulNamsanB"}>안녕하세요!</span>
+                <span className={"text-[1rem] mt-3 font-suite text-gray-300"}>
+                    나만의 마인크래프트 서버를 열고 강력히 관리해보세요.
+                </span>
+                <div className={"flex flex-row mt-5"}>
+                    <span className={"flex flex-row items-center transition-colors duration-150 ease-in-out bg-white hover:bg-gray-200 border-1 border-gray-400 text-gray-700 font-SeoulNamsanM text-[0.9rem] p-2 rounded-[5px] cursor-pointer"}><IoDocumentTextOutline className={"mb-1 mr-1"}/>튜토리얼 보기</span>
+                    <span className={"flex flex-row items-center transition-colors duration-150 ease-in-out bg-orange-400 hover:bg-orange-500 text-white font-SeoulNamsanM text-[0.9rem] p-2 rounded-[5px] ml-3 cursor-pointer"} onClick={() => {changeMenu(<AddServer/>)}}><FiPlus className={"mb-1 mr-1"}/>서버 생성하기</span>
+                </div>
+            </div>
         </div>
     );
 }

78623be9fa4e16f0403c70e0ad1bcbe95abb5623
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-08-02 11:06:42 +0900

    Main page 2차 틀잡기 시작

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,6 +1,10 @@
 
 export default function Main() {
     return (
-        <div className={"flex min-h-screen justify-center items-center text-3xl bold"}>Main</div>
+        <div className={"flex min-h-screen flex-col p-5"}>
+            <span className={"rounded-xl border-gray-600 border-2 h-auto p-3"}>
+                <span className={"h-5 text-2xl bold font-suite"}>안녕하세요!</span>
+            </span>
+        </div>
     );
 }

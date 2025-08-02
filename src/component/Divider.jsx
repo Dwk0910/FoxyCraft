6baf49d7ed6ef1f8f2a4d455065c5da0846d0fcd
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-08-02 10:40:29 +0900

    메뉴 전환 애니메이션 추가

--- src/component/Divider.jsx
+++
@@ -1,5 +0,0 @@
-
-export default function Divider(title) {
-    return (
-    );
-}
---
+++ src/page/Settings.jsx
@@ -0,0 +1,6 @@
+
+export default function Settings() {
+    return (
+        <div className={"flex min-h-screen justify-center items-center text-3xl bold"}>Settings</div>
+    );
+}

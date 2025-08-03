38a4a17b85e8d6f2f08ebde4747c2b105cb01fb6
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-08-03 21:13:11 +0900

    백엔드 호출 구문 추가
    - resources에 JRE 동봉
    - 실행환경과 프로세스에 맞는 JRE를 리소스에서 찾아 백엔드 JAR과 함꼐 실행하며, 실패했을 경우 dialog 출력
    - 로깅 강화

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, dialog, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
 const path = require('path');
 
 function createWindow() {
@@ -23,10 +24,66 @@ function createWindow() {
     }
 }
 
-app.whenReady().then(createWindow);
+// run new backend process
+let javaProcess = undefined;
+app.on('ready', () => {
+    const jarPath = path.join(__dirname, 'backend', 'FoxCraft.jar');
+    let javaExecutable = "";
+
+    if (app.isPackaged) {
+        if (process.platform === "win32") javaExecutable = path.join(process.resourcesPath, "jar_win", "bin", "java.exe");
+        else if (process.platform === "darwin") {
+            // macOS handle: 아키텍쳐에 따라 구분이 필요함
+            if (process.arch === "arm64") javaExecutable = path.join(process.resourcesPath, "jar_mac_arm64", "bin", "java"); // Apple Slicon
+            else if (process.arch === "x64") javaExecutable = path.join(process.resourcesPath, "jar_mac_x64", "bin", "java"); // Intel Mac
+        } else {
+            // 지원하지 않는 OS/Architecture
+            dialog.showMessageBoxSync({
+                type: "error",
+                title: "지원하지 않는 환경",
+                message: "죄송합니다. 이 환경에서는 실행이 불가능합니다.",
+                buttons: ["확인"]
+            });
+            app.quit();
+            return;
+        }
+    } else {
+        // 개발 모드에서는 시스템 기본 Java 사용
+        javaExecutable = "java";
+    }
+
+    console.log(`Starting Springboot App with ${javaExecutable}`);
+    const javaProcess = spawn(javaExecutable, ["-jar", jarPath]);
+
+    // console에 springboot app log출력
+    javaProcess.stdout.on('data', (data) => console.log(`[JAVA] ${data}`));
+    javaProcess.stderr.on('data', (data) => console.log(`[JAVA] ${data}`));
+    javaProcess.on('close', (code) => {
+        if (code !== 0) {
+            dialog.showMessageBoxSync({
+                type: "error",
+                title: "실행 중 오류 발생",
+                message: `앱 실행 중 오류가 발생했습니다. 앱 개발자에게 문의해 주십시오. Exit code : ${code}`,
+                buttons: ["확인"]
+            });
+            app.quit();
+            return;
+        }
+
+        console.log(`JavaProcess exited with code ${code}`);
+    });
+    createWindow();
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('will-quit', () => {
+    if (javaProcess) {
+        console.log("Terminating Java Process...");
+        javaProcess.kill();
+    }
 });
 
 app.on('activate', () => {

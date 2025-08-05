42ee1d193c50f934d4cd6bc6c294a16712d68247
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-05 17:43:38 +0900

    Compile 에러 수정

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    optimizeDeps: {
+        include: ['react-icons/io']
+    },
     plugins: [
         react(),
         tailwindcss()

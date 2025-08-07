18cccceef40fb9348d41ff42ddf9b96b50babb31
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-08 03:17:08 +0900

    [B] (File I/O) upload, cancel 추가

---
+++ backend/src/main/java/org/foxycraft/controller/UploadController.java
@@ -0,0 +1,53 @@
+package org.foxycraft.controller;
+
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import org.foxycraft.FoxyCraft;
+
+import java.io.File;
+import java.io.IOException;
+
+@CrossOrigin(origins = "http://localhost:5173", methods = RequestMethod.POST)
+@RestController
+@RequestMapping("/fileio")
+public class UploadController {
+    public static String uploadDir = System.getProperty("java.io.tmpdir") + File.separator + "foxycraft";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> onUpload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) return ResponseEntity.badRequest().body("File is empty");
+        try {
+            File targetFile = new File(uploadDir + File.separator + file.getOriginalFilename());
+            if (!targetFile.getParentFile().exists() && !targetFile.getParentFile().mkdirs()) throw new IOException("Could not create directory");
+            file.transferTo(targetFile);
+            return ResponseEntity.ok(targetFile.getAbsolutePath());
+        } catch (IOException e) {
+            FoxyCraft.logger.error(e);
+            return ResponseEntity.status(500).body("Internal Server Error.");
+        }
+    }
+
+    @PostMapping("/cancel")
+    public ResponseEntity<String> onCancel(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) return ResponseEntity.badRequest().body("File is empty");
+        try {
+            File targetFile = new File(uploadDir + File.separator + file.getOriginalFilename());
+            if (!targetFile.exists()) throw new IOException("Parent folder does not exist.");
+            if (!targetFile.delete()) throw new IOException("Could not delete file.");
+            return ResponseEntity.ok("success");
+        } catch (IOException e) {
+            FoxyCraft.logger.error(e);
+            return ResponseEntity.status(500).body("Internal Server Error.");
+        }
+    }
+}

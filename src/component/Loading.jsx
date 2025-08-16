9451d0934b195a611077dcd63989580d8ab666a9
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-17 04:34:32 +0900

    Modal 폰트 변경

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,19 +1,23 @@
-import { Modal } from "antd";
+import { ConfigProvider, Modal, theme } from "antd";
 
 export default function Loading({ loadingState }) {
     return (
-        <Modal
-            width={300}
-            closable={false}
-            maskClosable={false}
-            keyboard={false}
-            footer={null}
-            open={loadingState}
-            centered
-        >
-            <div className={"w-full flex justify-center"}>
-                <span className={"font-SeoulNamsanM text-[1rem]"}>로딩 중입니다...</span>
-            </div>
-        </Modal>
+        <ConfigProvider theme={{
+            algorithm: theme.darkAlgorithm
+        }}>
+            <Modal
+                width={300}
+                closable={false}
+                maskClosable={false}
+                keyboard={false}
+                footer={null}
+                open={loadingState}
+                centered
+            >
+                <div className={"w-full flex justify-center"}>
+                    <span className={"font-suite text-[1rem]"}>로딩 중입니다...</span>
+                </div>
+            </Modal>
+        </ConfigProvider>
     );
-}
+};

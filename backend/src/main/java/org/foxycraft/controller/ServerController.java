8c4e2f4e07e23dd8bc7d6622892a356b3ad23ce4
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-21 08:33:41 +0900

    WebSocket 틀짜기

--- backend/src/main/java/org/foxycraft/controller/ServerController.java
+++ backend/src/main/java/org/foxycraft/controller/ServerController.java
@@ -1,28 +1,37 @@
 package org.foxycraft.controller;
 
-import org.json.JSONObject;
-import org.springframework.http.ResponseEntity;
+import org.foxycraft.FoxyCraft;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.json.JSONArray;
-
-import org.foxycraft.Util;
-
 @RestController
-@RequestMapping("server")
+@RequestMapping("/server")
 @CrossOrigin(origins = "http://localhost:5173", methods = RequestMethod.POST)
 public class ServerController {
-    @PostMapping("start")
-    public ResponseEntity<String> startServer(@RequestParam("targetID") String targetUUID) {
-        JSONObject targetObject = Util.getServer(targetUUID);
-        if (targetObject == null) return ResponseEntity.badRequest().build();
-        Util.setServer(targetUUID, targetObject.put("status", "pending"));
-        return ResponseEntity.ok().build();
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public ServerController(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
     }
+
+    @MessageMapping("/msg")
+    public void message(@RequestParam("serverID") String id, @RequestParam("content") String content) {
+        messagingTemplate.convertAndSend("/subscription/serversocket/" + id, content);
+        FoxyCraft.logger.info("Content {} has been sent to room {}", content, id);
+    }
+
+//    @PostMapping("start")
+//    public ResponseEntity<String> startServer(@RequestParam("targetID") String targetUUID) {
+//        JSONObject targetObject = Util.getServer(targetUUID);
+//        if (targetObject == null) return ResponseEntity.badRequest().build();
+//        Util.setServer(targetUUID, targetObject.put("status", "pending"));
+//        return ResponseEntity.ok().build();
+//    }
 }

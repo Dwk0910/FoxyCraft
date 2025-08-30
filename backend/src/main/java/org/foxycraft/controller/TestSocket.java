adec86ac526996e265f4f267eb2915a598cbe853
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-08-30 17:52:40 +0900

    Test: 프로세스 테스트 완료

--- backend/src/main/java/org/foxycraft/controller/TestSocket.java
+++ backend/src/main/java/org/foxycraft/controller/TestSocket.java
@@ -1,7 +1,5 @@
 package org.foxycraft.controller;
 
-import io.methvin.watcher.DirectoryChangeEvent;
-import io.methvin.watcher.DirectoryWatcher;
 import org.foxycraft.FoxyCraft;
 
 import org.springframework.web.socket.CloseStatus;
@@ -10,37 +8,47 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.nio.file.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
 
 public class TestSocket extends TextWebSocketHandler {
-    public static DirectoryWatcher directoryWatcher;
-    public static Path testPath = Paths.get(FoxyCraft.dataPath);
-    public static Path targetPath = Paths.get(FoxyCraft.dataPath, "test.txt");
+    public static Process process;
+    public static volatile boolean isProcessRunning = false;
 
     @Override
     public void afterConnectionEstablished(@NotNull WebSocketSession session) {
-        FoxyCraft.logger.info("연결 감지됨. 감시를 시작합니다. (세션:{})", session);
+        FoxyCraft.logger.info("연결 감지됨. 프로세스를 시작합니다. (세션:{})", session);
 
-        try {
-            directoryWatcher = DirectoryWatcher.builder()
-                    .path(testPath)
-                    .listener(event -> {
-                        if (event.eventType() == DirectoryChangeEvent.EventType.MODIFY && event.path().equals(targetPath)) {
-                            session.sendMessage(new TextMessage(Files.readString(targetPath)));
-                            FoxyCraft.logger.info("{} MODIFIED", event.path());
-                        }
-                    })
-                    .build();
-            directoryWatcher.watchAsync();
-        } catch (IOException e) {
-            FoxyCraft.logger.error(e);
-        }
+        Thread thread = new Thread(() -> {
+            isProcessRunning = true;
+            try {
+                ProcessBuilder pb = new ProcessBuilder();
+                pb.command(List.of(Paths.get(FoxyCraft.resourcesPath, "jre_mac", "bin", "java").toString(), "-jar", Paths.get(FoxyCraft.resourcesPath, "test.jar").toString()));
+                process = pb.start();
+
+                session.sendMessage(new TextMessage("프로세스 시작됨, " + process.isAlive()));
+                BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    if (!isProcessRunning) break;
+                    FoxyCraft.logger.info("프로세스 출력: {}", line);
+                    session.sendMessage(new TextMessage(line));
+                }
+            } catch (IOException e) {
+                FoxyCraft.logger.error(e);
+            }
+        });
+        thread.start();
     }
 
     @Override
-    public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus status) throws Exception {
-        directoryWatcher.close();
+    public void afterConnectionClosed(@NotNull WebSocketSession session, @NotNull CloseStatus status) {
+        FoxyCraft.logger.info("연결 종료 감지됨. 프로세스를 종료합니다. (세션:{}, 상태:{})", session, status);
+        isProcessRunning = false;
     }
 }
